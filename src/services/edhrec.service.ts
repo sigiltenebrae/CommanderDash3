900ee8696aa03b040776419d61d7f1e2dc4dd310
sigiltenebrae <ck6202@rit.edu>
2022-08-05 19:21:57 -0400

    edhrec api started

--- src/services/edhrec.service.ts
+++ src/services/edhrec.service.ts
@@ -4,6 +4,7 @@ const EDHREC_STATIC_BASE = 'https://static.edhrec.com';
 const EDHREC_JSON_BASE = 'https://json.edhrec.com/v2';
 //https://cards.edhrec.com/atraxa-praetors-voice
 //https://json.edhrec.com/v2/commanders/atraxa-praetors-voice.json
+//https://json.edhrec.com/v2/commanders/brg.json
 const EDHREC_CARDS_BASE = 'https://cards.edhrec.com/';
 
 const COMMANDER_TYPEAHEAD_URL = EDHREC_STATIC_BASE + '/typeahead/commanders';
