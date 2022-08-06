c7c233c17f5e3e9fd0e5b7422fa84ee38215f568
sigiltenebrae <ck6202@rit.edu>
2022-08-06 00:47:03 -0400

    started deck create

--- src/app/deck-edit/deck-edit.component.ts
+++ src/app/deck-edit/deck-edit.component.ts
@@ -15,6 +15,8 @@ import {TokenStorageService} from "../../services/token-storage.service";
 export class DeckEditComponent implements OnInit {
   readonly  seperatorKeysCodes = [ENTER, COMMA] as const;
 
+  new_deck = false;
+
   current_user: any = null;
   current_deck: any = null;
   image_index = -1;
@@ -25,6 +27,8 @@ export class DeckEditComponent implements OnInit {
     partner_commander: null,
     friendly_name: null,
     deck_url: null,
+    image_url: null,
+    partner_image_url: null,
     play_rating: null,
     active: null,
     themes: null
@@ -34,27 +38,36 @@ export class DeckEditComponent implements OnInit {
               private deckData: DeckDataService, private token: TokenStorageService) {}
 
   ngOnInit(): void {
+
     const routeParams = this.route.snapshot.paramMap;
     const deckId = Number(routeParams.get('deckId'));
 
-    if (deckId < 0) {
+    if (deckId == -1) {
+      this.new_deck = true;
       this.current_deck = null;
+      this.form.friendly_name = "";
+      this.form.deck_url = "";
+      this.form.image_url = "";
+      this.form.play_rating = 1;
+      this.form.themes = [];
+      this.form.active = true;
+    }
+    else if (deckId < 0) {
+      this.router.navigate(['/']);
     }
     else {
-      this.deckData.getDecks().then((temp) => {
-        this.decks = temp;
-        this.deckData.getDeck(deckId).then((deck) => {
-        this.current_deck = deck;
-        this.form.commander = this.current_deck.commander;
-        this.form.partner_commander = this.current_deck.partner_commander;
-        this.form.friendly_name = this.current_deck.friendly_name;
-        this.form.deck_url = this.current_deck.deck_url;
-        this.form.play_rating = this.current_deck.play_rating;
-        this.form.active = this.current_deck.active;
-        this.form.themes = this.current_deck.themes;
-        this.form.image_url = this.current_deck.image_url;
-        this.image_index = 0;
-        });
+      this.deckData.getDeck(deckId).then((deck) => {
+      this.current_deck = deck;
+
+      this.form.commander = this.current_deck.commander;
+      this.form.partner_commander = this.current_deck.partner_commander;
+      this.form.friendly_name = this.current_deck.friendly_name;
+      this.form.deck_url = this.current_deck.deck_url;
+      this.form.play_rating = this.current_deck.play_rating;
+      this.form.active = this.current_deck.active;
+      this.form.themes = this.current_deck.themes;
+      this.form.image_url = this.current_deck.image_url;
+      this.image_index = 0;
       });
     }
   }
