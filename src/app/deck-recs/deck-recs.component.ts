5dda7e9676a0ed52f30770a468252a9d9e084b07
sigiltenebrae <ck6202@rit.edu>
2022-08-07 09:50:26 -0400

    recommendation theoretically done

--- src/app/deck-recs/deck-recs.component.ts
+++ src/app/deck-recs/deck-recs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 
-import {Subject, takeUntil, timer} from "rxjs";
+import {delay, Subject, takeUntil, timer} from "rxjs";
 import * as Scry from "scryfall-sdk";
 
 import {TokenStorageService} from "../../services/token-storage.service";
@@ -18,6 +18,8 @@ export class DeckRecsComponent implements OnInit {
   loading = false;
   calculating = false;
   calculated = false;
+  recommendation_count = 3;
+  recommendations: any[] = [];
 
   calc_clock: any;
   calc_clock_subscribe: any;
@@ -98,14 +100,20 @@ export class DeckRecsComponent implements OnInit {
       let sortedDeckList = this.sortDecks();
 
 
-      // sort
-      // grab the top x
-      // get the pictures for them
-      // get themes -> if partner get partner then themes
+      let recommendation_promises: any[] = [];
+      for (let j = 0; j < sortedDeckList.length; j++) {
+        if (j == this.recommendation_count) {
+          break;
+        }
+        recommendation_promises.push(this.getRecommendationData(sortedDeckList[j].commander));
+      }
+      Promise.all(recommendation_promises).then(() => {
+        console.log('done');
+        this.subject.next();
+        this.calculating = false;
+        this.calculated = true;
+      });
       // apply filter to themes
-      // select theme and subtheme
-      // display
-      this.subject.next();
     });
   }
 
@@ -114,13 +122,13 @@ export class DeckRecsComponent implements OnInit {
       let creator_promises: any[] = [];
 
       if (deck.active) {
-        this.http.get('/archidekt/api/decks/cards/?deckFormat=3&commanders="' + deck.commander + '"&orderBy=-viewCount&pageSize=100').subscribe((archidekt_decks) => {
+        this.http.get('/archidekt/api/decks/cards/?deckFormat=3&commanders="' + deck.commander + '"&orderBy=-viewCount&pageSize=100').pipe(delay(500)).subscribe((archidekt_decks) => {
           let linked_decks: any = archidekt_decks;
           if (deck.partner_commander) {
             linked_decks.results.forEach((linked_deck: any) => {
               if ((Math.random() * 100) > this.user_randomness ) { creator_promises.push(this.getDecksForCreator(linked_deck.owner.username, deck.play_rating / 2)); }
             });
-            this.http.get('/archidekt/api/decks/cards/?deckFormat=3&commanders="' + deck.commander + '"&orderBy=-viewCount&pageSize=100').subscribe((archidekt_partner_decks) => {
+            this.http.get('/archidekt/api/decks/cards/?deckFormat=3&commanders="' + deck.commander + '"&orderBy=-viewCount&pageSize=100').pipe(delay(500)).subscribe((archidekt_partner_decks) => {
               let linked_partner_decks: any = archidekt_partner_decks;
               linked_partner_decks.results.forEach((linked_partner_deck: any) => {
                 if ((Math.random() * 100) > this.user_randomness ) { creator_promises.push(this.getDecksForCreator(linked_partner_deck.owner.username, deck.play_rating / 2)); }
@@ -132,7 +140,7 @@ export class DeckRecsComponent implements OnInit {
             linked_decks.results.forEach((linked_deck: any) => {
               if ((Math.random() * 100) > this.user_randomness ) { creator_promises.push(this.getDecksForCreator(linked_deck.owner.username, deck.play_rating)); }
             });
-            Promise.all(creator_promises).then(() => { this.commander_position++; resolve_commander(); }).catch((err) => { console.log(err); resolve_commander(); });
+            Promise.all(creator_promises).then(() => { this.commander_position++; console.log(deck.commander + ' done.'); resolve_commander(); }).catch((err) => { console.log(err); resolve_commander(); });
           }
         });
       }
@@ -143,7 +151,7 @@ export class DeckRecsComponent implements OnInit {
     return new Promise<void>((resolve_user) => {
       let recommended_promises: any[] = [];
 
-      this.http.get('/archidekt/api/decks/cards/?owner=' + username + '&orderBy=-viewCount').subscribe((recommend_decks) => {
+      this.http.get('/archidekt/api/decks/cards/?owner=' + username + '&orderBy=-viewCount').pipe(delay(500)).subscribe((recommend_decks) => {
         let recommended_decks: any = recommend_decks;
 
         recommended_decks.results.forEach((recommended_deck: any) => { recommended_promises.push(this.getDeckFromServer(recommended_deck.id, playRating)); });
@@ -154,7 +162,7 @@ export class DeckRecsComponent implements OnInit {
 
   async getDeckFromServer(deckId: number, playRating: number) {
     return new Promise<void>((resolve_deck) => {
-      this.http.get('/archidekt/api/decks/' + deckId + '/').subscribe((archidektDeckInfo) => {
+      this.http.get('/archidekt/api/decks/' + deckId + '/').pipe(delay(500)).subscribe((archidektDeckInfo) => {
         let deckInfo: any = archidektDeckInfo;
         deckInfo.cards.forEach(async (card: any) => {
           if (card.categories.includes("Commander")) {
@@ -257,5 +265,106 @@ export class DeckRecsComponent implements OnInit {
     });
     sorted_recomendations.sort((a, b) => (b.score > a.score) ? 1 : -1);
     return sorted_recomendations;
+  }
+
+  async getRecommendationData(commander: string) {
+    return new Promise<void>(async (resolve_recommendation) => {
+      let outData: any = {};
+      outData.commander = commander;
+      let cur = await Scry.Cards.byName(commander);
+      let cur_prints = await cur.getPrints();
+      if (cur_prints) {
+        outData.image_url = cur_prints[0].image_uris?.png;
+      }
+      else {
+        outData.image_url = '';
+      }
+      let edhrec_name = commander.toLowerCase().replace(/[`~!@#$%^&*()_|+=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/\ /g, '-');
+      this.http.get('https://json.edhrec.com/v2/commanders/' + edhrec_name + '.json').subscribe(async (edhrec_json) => {
+        let edh_data: any = edhrec_json;
+        if (edh_data.panels.partnercounts) {
+          let partners: any[] = [];
+          edh_data.panels.partnercounts.forEach((partner: any) => {
+            partners.push(partner.alt);
+          });
+          outData.partner = (this.assignThemeWeights(partners)[0]);
+          if (outData.partner) {
+            let cur_partner = await Scry.Cards.byName(outData.partner);
+            let cur_partner_images = await cur_partner.getPrints();
+            if (cur_partner_images) {
+              outData.partner_image_url = cur_partner_images[0].image_uris?.png;
+            }
+            else {
+              outData.partner_image_url = '';
+            }
+          }
+          edhrec_name += outData.partner.toLowerCase().replace(/[`~!@#$%^&*()_|+=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/\ /g, '-');
+          this.http.get('https://json.edhrec.com/v2/commanders/' + edhrec_name + '.json').subscribe((edhrec_json_partner) => {
+              edh_data = edhrec_json_partner;
+          }, (err) => {
+            console.log(err);
+            outData.theme_rec = '';
+            this.recommendations.push(outData);
+            resolve_recommendation();
+          });
+        }
+
+        let themes_list: any[] = [];
+        if (edh_data.panels.links) {
+          edh_data.panels.links[2].items.forEach((theme_option: any) => {
+            themes_list.push(theme_option.value);
+          });
+          outData.theme_rec = this.assignThemeWeights(themes_list);
+        }
+        else {
+         outData.theme_rec = '';
+        }
+        this.recommendations.push(outData);
+        resolve_recommendation();
+      }, (err) => {
+        console.log(err);
+        outData.theme_rec = '';
+        this.recommendations.push(outData);
+        resolve_recommendation();
+      });
+    });
+  }
+
+  assignThemeWeights(themes: string[]) {
+    if (themes) {
+      let weighted_themes: any = {};
+      let x_coeff = 0;
+      let factor = (1 - ((this.theme_randomness) / 100));
+      for (let i = 0; i < themes.length; i++) {
+        x_coeff += Math.pow(factor, i);
+      }
+      let base = 100 / x_coeff;
+      let cur_coeff = 0;
+      for(let i = 0; i < themes.length; i++) {
+        cur_coeff += Math.pow(factor, i);
+        weighted_themes[themes[i]] = Math.floor(100 - ((cur_coeff - 1) * base)) / 100;
+      }
+      //return weighted_themes;
+      let weighted_themes_list: any[] = [];
+      Object.keys(weighted_themes).forEach((theme_name) => {
+        weighted_themes_list.push({ theme: theme_name, weight: weighted_themes[theme_name] });
+      });
+      weighted_themes_list.sort((a: any, b: any) => (b.weight > a.weight) ? 1 : -1);
+      return weighted_themes_list;
+    }
+    else {
+      return [''];
+    }
+
+  }
+
+  secondsToString(all_seconds: number) {
+    let seconds: string | number = Math.floor(all_seconds % 60)
+    let minutes: string | number = Math.floor( (all_seconds / 60) % 60)
+    let hours: string | number = Math.floor((all_seconds / (60 * 60)) % 60)
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    hours = (hours < 10) ? '0' + hours : hours;
+    return `${hours}:${minutes}:${seconds}`;
   }
 }
