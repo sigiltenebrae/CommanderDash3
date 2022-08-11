cada1e37c704092d89f9c1c614fb5de24a555bc7
sigiltenebrae <ck6202@rit.edu>
2022-08-11 15:21:26 -0400

    environment updated for public use

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+let mtg_api_url = 'https://example.com/api'
+
+export const environment = {
+  production: false,
+  users_url: mtg_api_url + '/users',
+  decks_url: mtg_api_url + '/decks/',
+  deck_themes_url: mtg_api_url + '/deckthemesname/',
+  themes_url: mtg_api_url + '/themes',
+
+  auth_url: mtg_api_url + '/auth/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
